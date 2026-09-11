6efe28b16c7c892867a258612b5b531f0b280e83
Rayhan <sigmaperson2828@gmail.com>
2026-09-12 02:33:54 +0700

    style(lighting): reposition filament line as elegant diagonal ribbon connecting both feature cards

--- src/components/BackgroundGlow.tsx
+++ src/components/BackgroundGlow.tsx
@@ -33,13 +33,13 @@ export function BackgroundGlow() {
           }}
         />
 
-        {/* 2. Linear/Vercel-style refined filament lighting (Centered on 1440px canvas, top: 1120px) */}
+        {/* 2. Linear/Vercel-style diagonal filament ribbon connecting both feature cards */}
         <img
           src="/assets/aurora.svg"
           alt=""
           width={1440}
-          height={1400}
-          className="pointer-events-none absolute left-0 top-[1120px] h-[1400px] w-[1440px] select-none mix-blend-screen"
+          height={700}
+          className="pointer-events-none absolute left-0 top-[1100px] h-[700px] w-[1440px] select-none mix-blend-screen"
         />
       </div>
     </div>
